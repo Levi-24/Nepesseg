e18f0c1b931cef9c85fdad73d3d9c11136fb72ce
Levente <122294490+Levi-24@users.noreply.github.com>
2023-10-25 13:50:59 +0200

    Initial commit

---
+++ Orszag.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nepesseg
+{
+    class Orszag
+    {
+        public string Nev { get; set; }
+        public int Terulet { get; set; }
+        public double Nepesseg { get; set; }
+        public string Fovaros { get; set; }
+        public int FovarosNepesseg { get; set; }
+
+        public Orszag(string sor)
+        {
+            var darabok = sor.Split(';');
+            this.Nev = darabok[0];
+            this.Terulet = int.Parse(darabok[1]);
+            if (darabok[2].Contains('g'))
+            {
+                string szam = darabok[2].Replace("g", "0000");
+                this.Nepesseg = double.Parse(szam);
+            }
+            else
+                this.Nepesseg = double.Parse(darabok[2]);
+            this.Fovaros = darabok[3];
+            this.FovarosNepesseg = int.Parse(darabok[4]);
+        }
+
+        public int Nepsuruseg()
+        {
+            return 142;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Nepesseg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Orszag> orszagok = new List<Orszag>();
+            using StreamReader reader = new StreamReader(
+                path: @"..\..\..\src\adatok-utf8.txt",
+                Encoding.UTF8);
+
+            _ = reader.ReadLine();
+            while (!reader.EndOfStream)
+                orszagok.Add(new Orszag(reader.ReadLine()));
+
+            Console.WriteLine($"3.Feladat: {orszagok.Count()}");
+
+
+        }
+    }
+}
